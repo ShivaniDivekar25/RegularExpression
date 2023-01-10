1f19897cc93524cdc40d93115c74497a405a4c7e
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-10 23:13:14 +0530

    Modified to Restrict Alphabate At Beginning Of Pin Code program

--- RegularExpressionProgram/Program.cs
+++ RegularExpressionProgram/Program.cs
@@ -6,11 +6,14 @@
         {
             Console.WriteLine("Welcome to Regular Expression program");
             Console.WriteLine("Please enter the option given below");               //Taking user input
-            Console.WriteLine("1:Create PIN code");
+            Console.WriteLine("1:Create PIN code\n2:Restrict alphabates at biginning of Pin Code");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)             //Create switch case to call different methods
             {
                 case 1:
+                    RegularExpression.validatePinCode();
+                    break;
+                case 2:
                     RegularExpression.validatePinCode();
                     break;
                 default:
