f3c52ee800fc50d11ea6ad3c5f78be74fb12d0b8
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-10 23:03:50 +0530

    Modified to Create Pin Code program

--- RegularExpressionProgram/Program.cs
+++ RegularExpressionProgram/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regular Expression program");
+            Console.WriteLine("Please enter the option given below");               //Taking user input
+            Console.WriteLine("1:Create PIN code");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)             //Create switch case to call different methods
+            {
+                case 1:
+                    RegularExpression.validatePinCode();
+                    break;
+                default:
+                    Console.WriteLine("Plaese choose correct option");
+                    break;
+            }
+
         }
     }
 }
---
+++ RegularExpressionProgram/RegularExpression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionProgram
+{
+    internal class RegularExpression
+    {
+        public static void validatePinCode()
+        {
+            string pattern = "^[0-9]{6}$";
+            string[] arr = { "400088" };
+            TestPattern(pattern, arr);
+        }
+        public static void TestPattern(string pattern, string[] arr)
+        {
+            foreach (string inputs in arr)
+            {
+                if (Regex.IsMatch(inputs, pattern))
+                {
+                    Console.WriteLine("{0} is valid", inputs);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is invalid", inputs);
+                }
+            }
+        }
+    }
+}
